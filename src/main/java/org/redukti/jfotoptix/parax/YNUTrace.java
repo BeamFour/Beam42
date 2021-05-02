fec5efc8c8741c4d9787e7a26d781b9a30897263
Dibyendu Majumdar <mobile@majumdar.org.uk>
2021-05-02 22:43:11 +0100

    Add the image plane data

--- src/main/java/org/redukti/jfotoptix/parax/YNUTrace.java
+++ src/main/java/org/redukti/jfotoptix/parax/YNUTrace.java
@@ -26,6 +26,7 @@ public class YNUTrace {
         double y1_ = y1;
         double u1 = initial_angle;  // angle
         double y2 = y1;
+        double aoi = 0.0;
         for (Element e: seq) {
             if (e instanceof OpticalSurface) {
                 y1 = y2; // height on this surface
@@ -40,7 +41,7 @@ public class YNUTrace {
                 // Calculate y for next surface
                 y2 = y1 + t1 * (n1_u1_)/n1_;    // Eq 56 in MIL-HDBK-141, Eq 2.32 in MOE
                 u1 = n1_u1_/n1_; // ray angle
-                double aoi = u1 + y1 * C1;
+                aoi = u1 + y1 * C1; // Eq 1.51 in handbook of Optical Dsign
                 double power = surface.power(SpectralLine.d);
                 System.out.println("id="+ e.id() + new Vector3(y1, u1, aoi).toString());
                 tracedata.put(e.id(), new Vector3(y1, u1, aoi));
@@ -49,9 +50,12 @@ public class YNUTrace {
                 continue;
             }
         }
-        double l = -y1/u1;
-        System.out.println(l);
-        System.out.println(-1.0/u1);
+        // FIXME we should add this against the image surface
+        tracedata.put(0, new Vector3(y2, u1, aoi)); // For the image plane, but perhaps we should make Image an OpticalSurface
+        //double l = -y1/u1;
+        //System.out.println(l);
+        //System.out.println(-1.0/u1);
+        System.out.println("Image id=0,"+tracedata.get(0));
         return tracedata;
     }
 

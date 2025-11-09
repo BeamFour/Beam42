e80429ef8064f520bc9097c8aa8b2845a2edce3c
dibyendumajumdar <mobile@majumdar.org.uk>
2025-11-09 17:21:00 +0000

    Update from rayoptics4j

--- rayoptics/src/main/java/org/redukti/rayoptics/raytr/RayDataWithDist.java
+++
@@ -1,9 +0,0 @@
-// Copyright 2017-2015 Michael J. Hayford
-// Original software https://github.com/mjhoptics/ray-optics
-// Java version by Dibyendu Majumdar
-package org.redukti.rayoptics.raytr;
-
-public class RayDataWithDist {
-    public RayData ray_data;
-    public double z_enp;
-}
--- rayoptics/src/main/java/org/redukti/rayoptics/raytr/TraceWithOPDResult.java
+++
@@ -1,8 +0,0 @@
-// Copyright 2017-2015 Michael J. Hayford
-// Original software https://github.com/mjhoptics/ray-optics
-// Java version by Dibyendu Majumdar
-package org.redukti.rayoptics.raytr;
-
-public class TraceWithOPDResult {
-
-}

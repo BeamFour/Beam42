b0fc0b7a16b7c49010a7e5ffa493b70cebc4a71e
dibyendumajumdar <mobile@majumdar.org.uk>
2025-12-14 18:53:41 +0000

    try out kidger's mtf goal

---
+++ optimr/src/main/java/org/redukti/optim/GoalMTF.java
@@ -0,0 +1,43 @@
+package org.redukti.optim;
+
+import org.redukti.rayoptics.util.Lists;
+
+/**
+ * Ray aberration for a field / xy / pos / wvl.
+ */
+public class GoalMTF extends Goal {
+    public final int field;
+    public final int xy;
+    public final int pos;
+    public final double wvl;
+    public final double freq;
+    public GoalMTF(Analysis analysis, int field, int xy, int pos, double wvl, double freq, double target, double weight) {
+        super(analysis,target,weight);
+        this.field = field-1;
+        this.xy = xy;
+        this.pos = pos;
+        this.wvl = wvl;
+        this.freq = freq;
+    }
+
+    @Override
+    public double value() {
+        var fans = analysis.ray_aberrations.get_fans(field,xy,wvl);
+        if (fans != null) {
+            double aberration = 0;
+            try {
+                aberration = Lists.get(fans.fan_y, pos);
+            }
+            catch (Exception e) {
+                return 1.0;
+            }
+            return Math.sin(Math.PI * freq * aberration);
+        }
+        throw new IllegalArgumentException("Invalid field, xy or position");
+    }
+
+    @Override
+    public String toString() {
+        return "RayAberration field=" + field + ", xy=" + xy + ", pos=" + pos + ", target=" + target + ", weight=" + weight + " = " + value();
+    }
+}

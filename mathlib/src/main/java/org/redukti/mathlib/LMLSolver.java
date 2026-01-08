f722974ab4352fd9bdc958d8f832a77a44b15552
dibyendumajumdar <mobile@majumdar.org.uk>
2026-01-08 00:10:52 +0000

    Increase max iterations to 150

--- mathlib/src/main/java/org/redukti/mathlib/LMLSolver.java
+++ mathlib/src/main/java/org/redukti/mathlib/LMLSolver.java
@@ -39,7 +39,7 @@ public class LMLSolver {
 
     private LMLFunction myH = null;    // overwritten by constructor
     private double  lmtol = 1E-6;  // overwritten by constructor
-    private int     lmiter = 100;  // overwritten by constructor
+    private int     lmiter = 150;  // overwritten by constructor
     private int     nparms = 0;    // overwritten by constructor
     private int     npts = 0;      // overwritten by constructor
 

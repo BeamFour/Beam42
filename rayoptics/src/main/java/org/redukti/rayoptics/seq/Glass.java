60963f4584f4b92670210715223b96ac48c1a3a7
dibyendumajumdar <mobile@majumdar.org.uk>
2026-05-16 23:39:13 +0100

    Add nikkor 200mm f2 afs

--- rayoptics/src/main/java/org/redukti/rayoptics/seq/Glass.java
+++ rayoptics/src/main/java/org/redukti/rayoptics/seq/Glass.java
@@ -514,6 +514,8 @@ public class Glass extends Medium {
         glasses.put("PC102B", new Glass("Hikari", "PC102B", 1.50032, 1.498456, 1.504565, 1.501779, 1.49875, 1.504909, 1.507857, 81.9, 81.48, 0.0));
         // Following is an attempt to reconstruct Nikon PC102 glass based on vd,nd and offset from FK50
         glasses.put("PC102C", new Glass("Hikari", "PC102C", 1.50032, 1.498458, 1.504567, 1.501783, 1.498754, 1.504904, 1.507851, 81.9, 81.48, 0.0));
+        glasses.put("FKH2-Proto", new Glass("Hikari", "FKH2-Proto", 1.44679, 1.445285, 1.450194, 1.447961, 1.445521, 1.450470, 1.452821, 91.0, 81.48, 0.0));
+
     }
     static void add_hoya_glasses() {
 // Hoya

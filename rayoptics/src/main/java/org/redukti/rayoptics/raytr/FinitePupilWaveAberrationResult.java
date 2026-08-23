6e7e2afd17344805d3c0247ffa518ea3865e42e9
dibyendumajumdar <mobile@majumdar.org.uk>
2026-08-23 15:52:21 +0100

    Add comments

--- rayoptics/src/main/java/org/redukti/rayoptics/raytr/FinitePupilWaveAberrationResult.java
+++ rayoptics/src/main/java/org/redukti/rayoptics/raytr/FinitePupilWaveAberrationResult.java
@@ -2,6 +2,26 @@ package org.redukti.rayoptics.raytr;
 
 import org.redukti.mathlib.Vector3;
 
-public record FinitePupilWaveAberrationResult(RayPkg ray_pkg, ChiefRayPkg chief_ray_pkg, ReferenceSphere ref_sphere,
-                                              double e1, double ekp, double ep, Vector3 ray_exit_pupil_pt, double ray_op, double cr_op ) {
+/**
+ *
+ * @param ray_pkg A test aperture ray
+ * @param chief_ray_pkg The chief ray
+ * @param ref_sphere The reference sphere
+ * @param e1 Object space geometric difference between chief ray and aperture ray from entry pupil to first surface.
+ * @param ekp Image space geometric difference between chief ray amd aperture ray from last surface to exit pupil excluding impact of focus.
+ * @param ep Image space geometric adjustment for focus
+ * @param ray_exit_pupil_pt Intersection of aperture ray with exit pupil reference sphere
+ * @param ray_op optical path length of aperture ray inside the lens
+ * @param cr_op optical path length of chief ray inside the lens
+ */
+public record FinitePupilWaveAberrationResult(
+        RayPkg ray_pkg,
+        ChiefRayPkg chief_ray_pkg,
+        ReferenceSphere ref_sphere,
+        double e1,
+        double ekp,
+        double ep,
+        Vector3 ray_exit_pupil_pt,
+        double ray_op,
+        double cr_op ) {
 }
